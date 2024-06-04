83d649fc13f5bd386c9b0408074e293355d51fc4
Jhow-Magnum <jhowmagnumweb@outlook.com>
2024-06-03 21:07:38 -0300

    Criteria_Projeto_ScreenSound

---
+++ src/main/java/br/com/music/screensound/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.music.screensound.model;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA;
+}
---
+++ src/main/java/br/com/music/screensound/service/OpenAiService.java
@@ -0,0 +1,2 @@
+package br.com.music.screensound.service;public class OpenAiService {
+}
